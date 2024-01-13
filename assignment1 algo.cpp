35915e7eae026713b9bda35db0a4fd78b5dc6076
Sabir Khosa <sabirhussaink2001@gmail.com>
2024-01-14 00:25:22 +0500

    comments added in the code assignment1 algo.cpp

--- assignment1 algo.cpp
+++ assignment1 algo.cpp
@@ -3,63 +3,62 @@
 #include<sstream>
 using namespace std;
 
+// Function to convert an integer to a string
 string int_to_string(int x){
-	stringstream ss;
-	ss <<x;
-	return ss.str();
+    stringstream ss;
+    ss << x;
+    return ss.str();
 }
 
+// Function to add a zero to the end of an integer and return it as a string
 string add_zero(int x){
-	string str1=int_to_string(x);
-	char str2='0';
-	str1+=str2;
-	return str1;
+    string str1 = int_to_string(x);
+    char str2 = '0';
+    str1 += str2;
+    return str1;
 }
 
+// Function to convert a string to an integer
 int string_to_int(string x){
-	stringstream degree(x);
-	int y=0;
-	degree>>y;
-	return y;
+    stringstream degree(x);
+    int y = 0;
+    degree >> y;
+    return y;
 }
 
-
-int power(int x,int y){
-	if(y==0){
-		return 1;
-	}
-	
-	return x*power(x,y-1);
+// Function to calculate x raised to the power y
+int power(int x, int y){
+    if(y == 0){
+        return 1;
+    }
+    return x * power(x, y - 1);
 }
 
-  int function(int x,int y){
-	int shift,result;
-	string str1=add_zero(x);
-//	string s=int_to_string(x);
-	//string str1=add_zero()
-	int num1=string_to_int(str1);
-	return num1;
- }
-
-
-
-
-
+// Function that seems to concatenate a zero to the end of x
+int function(int x, int y){
+    // Add a zero to the end of x
+    string str1 = add_zero(x);
+    // Convert the modified string back to an integer
+    int num1 = string_to_int(str1);
+    return num1;
+}
 
 int main(){
-	int x,y;
-	cout<<"enter value of x :";
-	cin>>x;
-	cout<<"enter value of y :";
-	cin>>y;
-	
-	//int z=function(x,y);
-	int a=power(x,y);
-	cout<<"power is "<<a<<endl;
-	int b=function(x,y);
-	cout<<"after conacatenation is "<<b;
-//	int z=add_zero(x);
-//	cout<<"after adding zero is "<<z;
-	
-	return 0;
+    int x, y;
+
+    // Get input values for x and y
+    cout << "Enter the value of x: ";
+    cin >> x;
+    cout << "Enter the value of y: ";
+    cin >> y;
+
+    // Calculate x^y using recursion
+    int a = power(x, y);
+    cout << "Power is: " << a << endl;
+
+    // Call the function that adds a zero to the end of x and converts it to an integer
+    int b = function(x, y);
+    cout << "After concatenation: " << b;
+
+    return 0;
 }
