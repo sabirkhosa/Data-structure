2589540318c52fdcae878aa670198aa83246bad7
Sabir Khosa <sabirhussaink2001@gmail.com>
2024-01-14 01:01:07 +0500

    problem to reverse a string using recursion.cpp

--- problem lab task 3.cpp
+++ problem lab task 3.cpp
@@ -2,22 +2,31 @@
 #include<string>
 using namespace std;
 
-string reverse(string str,char special,int index=0){
-    string str1="";
-    if(str[index]==special){ 
-      return str1;
+// Function to reverse a string until a special character is encountered
+string reverse(string str, char special, int index = 0) {
+    string str1 = "";
+
+    // Base case: stop recursion when the special character is encountered
+    if (str[index] == special) {
+        return str1;
+    } else {
+        // Recursive call: reverse the remaining part of the string
+        str1 += reverse(str, special, index + 1);
+
+        // Append the current character to the reversed string
+        str1 += str[index];
+
+        // Return the reversed string
+        return str1;
     }
-    else{
-    str1+= reverse(str,special,index+1);
-    str1 += str[index];
-    return str1;
-    
 }
 
-}
+int main() {
+    // Example usage: reverse the string "Hello Dorothy$ nice to see you" until '$'
+    string s = reverse("Hello Dorothy$ nice to see you", '$', 0);
 
-int main(){
-    string s = reverse("Hello Dorothy$ nice to see you",'$',0);
-    cout<<s<<endl;
+    // Print the reversed string
+    cout << s << endl;
+
     return 0;
 }
