b4d347553e41f6279c6155611b1b3894f9175c86
Sabir Khosa <sabirhussaink2001@gmail.com>
2024-01-14 00:36:19 +0500

    problem to search elemement in array whose sum is equal to target.cpp

--- ds lab task 2.cpp
+++ ds lab task 2.cpp
@@ -1,44 +1,59 @@
 #include<iostream>
 using namespace std;
 
+int main() {
+    // Define the size of the array
+    int size = 6;
 
-int main(){
-	int size=6;
-	int *arr=new int[size];
-	int *ptr=NULL;
-	int *ptr1 = NULL;
-	ptr=arr;
-	cout<<"enter the values of array :"<<endl;
-	for(int i=0; i<size; i++){
-		cin >> *ptr;
-		ptr++;
-	}
-	cout<<"array_list = ";
-	ptr = arr;
-	for (int i =0 ; i<size ;i ++){
-		cout<<*ptr <<" ";
-		ptr++;
-	}
-	int target = 15;
-	ptr1 = ptr = arr;
-	int sum;
-	int flag = 0;
-	for(int i = 0 ; i < size ; i++){
-		for (int j = 0 ; j < size ; j++){
-			sum = *ptr + *ptr1;
-			if (sum == target){
-				flag = 1;
-				break;
-			}
-			ptr1++;
-		}
-		if (flag == 1){
-			break;
+    // Create a dynamic array of integers
+    int *arr = new int[size];
+    int *ptr = NULL;
+    int *ptr1 = NULL;
+    ptr = arr;
 
-		}
-		ptr1 = arr;
-		ptr++;		
-	}
-	cout<<endl<<"output : "<< *ptr << "," << *ptr1 <<endl;
-	return 0;
+    // Input values into the array
+    cout << "Enter the values of the array:" << endl;
+    for (int i = 0; i < size; i++) {
+        cin >> *ptr;
+        ptr++;
+    }
+
+    // Print the input array
+    cout << "Array list: ";
+    ptr = arr;
+    for (int i = 0; i < size; i++) {
+        cout << *ptr << " ";
+        ptr++;
+    }
+
+    // Set the target sum to find
+    int target = 15;
+    ptr1 = ptr = arr;
+    int sum;
+    int flag = 0;
+
+    // Iterate through the array to find two elements whose sum is equal to the target
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            sum = *ptr + *ptr1;
+            if (sum == target) {
+                flag = 1;
+                break;
+            }
+            ptr1++;
+        }
+        if (flag == 1) {
+            break;
+        }
+        ptr1 = arr;
+        ptr++;
+    }
+
+    // Print the output, i.e., the two elements whose sum is equal to the target
+    cout << endl << "Output: " << *ptr << ", " << *ptr1 << endl;
+
+    // Deallocate memory for the dynamic array
+    delete[] arr;
+
+    return 0;
 }
