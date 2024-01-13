3dcc525bac7a299be78ac0333b28ae166dfc676e
Sabir Khosa <sabirhussaink2001@gmail.com>
2024-01-14 00:53:19 +0500

    function to find most frequent element in array .cpp

--- lab assignment.cpp
+++ lab assignment.cpp
@@ -1,80 +1,68 @@
 #include<iostream>
 using namespace std;
 
-class list{
-	public: 
-		int *arr,*temp,*a;
-		int x,y,size;
-		list(){
-			arr=temp=NULL;
-		}
-		void MostFrequentElementInSubArray(int left, int right, int freq){
-			cout<<"enter the size of array: ";
-			cin>>size;
-			arr=new int[size];
-			temp=arr;
-			for(int i=0; i<size; i++){
-				cin>>*temp;
-				temp++;
-			}
-			
-			a = new int[right];
-			for(int i = 0 ; i< right ; i++){
-				a[i] = NULL;
-			}
-			
-			int counter = 0;
-			int index = 0;
-			int flag = 0;
-			for (int i = 0 ; i <= right ; i++){
-				for (int k = 0 ; k < right ; k++){
-					if(a[k] == arr[i]){
-						flag = 1;
-						break;
-					}
-				}
-				if (flag == 1){
-					flag = 0;
-					continue;
-					
-				}
-				for (int j = 0 ; j <= right ; j++){
-					if (arr[i] == arr[j]){
-						counter++;
-					}
-					if (counter >= freq){
-						a[index] = arr[i];
-						counter = 0;
-						index++;
-					}
-				}
-			}
-			
-		for (int i = 0 ; i < right ; i++){
-			if (a[i] == NULL){
-				break;
-		}else{
-			cout<<"output : "<<a[i]<<endl;
-		}
-		}
-			
-		}
-		
-};
+class list {
+public:
+    int *arr, *temp, *a;
+    int x, y, size;
 
-int main(){
-	/*
-	list obj;
-	int left,right,freq;
-	cout<<"left ";
-	cin>>left;
-	cout<<"right ";
-	cin>>right;
-	cout<<"frequent ";
-	cin>>freq;
-	obj.MostFrequentElementInSubArray(left,right,freq);
-	return 0;
-	*/
-	int a=8;
-	cout<<"after divide "<<10/10;
-}
+    list() {
+        arr = temp = NULL;
+    }
+
+    // Function to find the most frequent element in a subarray
+    void MostFrequentElementInSubArray(int left, int right, int freq) {
+        cout << "Enter the size of the array: ";
+        cin >> size;
+
+        arr = new int[size];
+        temp = arr;
+
+        // Input elements into the array
+        cout << "Enter array elements: ";
+        for (int i = 0; i < size; i++) {
+            cin >> *temp;
+            temp++;
+        }
+
+        // Array to store unique elements meeting the frequency condition
+        a = new int[right];
+        for (int i = 0; i < right; i++) {
+            a[i] = NULL;
+        }
+
+        int counter = 0;
+        int index = 0;
+        int flag = 0;
+
+        // Iterate through the array to find most frequent elements in the subarray
+        for (int i = 0; i <= right; i++) {
+            for (int k = 0; k < right; k++) {
+                if (a[k] == arr[i]) {
+                    flag = 1;
+                    break;
+                }
+            }
+
+            if (flag == 1) {
+                flag = 0;
+                continue;
+            }
+
+            for (int j = 0; j <= right; j++) {
+                if (arr[i] == arr[j]) {
+                    counter++;
+                }
+
+                if (counter >= freq) {
+                    a[index] = arr[i];
+                    counter = 0;
+                    index++;
+                }
+            }
+        }
+
+        // Output the most frequent elements in the subarray
+        cout << "Output: ";
+        for (int i = 0; i < right; i++) {
+            if 
