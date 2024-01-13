847ed6f676855ae8e326e8a73e853ef978ca4dff
Sabir Khosa <sabirhussaink2001@gmail.com>
2024-01-14 00:38:56 +0500

    problem to find a decimal point in a given string.cpp

--- ds lab1 task.cpp
+++ ds lab1 task.cpp
@@ -1,45 +1,54 @@
 #include<iostream>
 using namespace std;
-int getlength(char *ch,char *ptr,int count){
-	if (*ptr=='\0'){
-			return count;
-}
-		
-		else{
-			
-			ptr++;
-			count=count+1;
-			
-	return	getlength(ch,ptr,count);
-		
-			
-	}
-	
 
-}
-int finddecimal(char *ch,char *ptr,int index){
-	if (*ptr=='.'){
-		return index;
-	}
-	else{
-		ptr++;
-		index=index+1;
-		return finddecimal(ch,ptr,index);
-	}
+// Function to calculate the length of a string
+int getlength(char *ch, char *ptr, int count) {
+    // Check if the current character is the null terminator '\0'
+    if (*ptr == '\0') {
+        // Return the count as the length of the string
+        return count;
+    } else {
+        // Move to the next character in the string and increment the count
+        ptr++;
+        count = count + 1;
+
+        // Recursively call the function to process the next character
+        return getlength(ch, ptr, count);
+    }
 }
 
+// Function to find the position of the decimal point in a string
+int finddecimal(char *ch, char *ptr, int index) {
+    // Check if the current character is a decimal point '.'
+    if (*ptr == '.') {
+        // Return the index as the position of the decimal point
+        return index;
+    } else {
+        // Move to the next character in the string and increment the index
+        ptr++;
+        index = index + 1;
 
-int main(){
+        // Recursively call the function to find the decimal point
+        return finddecimal(ch, ptr, index);
+    }
+}
+
+int main() {
+    // Define a character array (string)
     char *ch = "22222.222222222";
-    char *ptr=NULL;
-    ptr=ch;
-	int x;
-	int y;
-	int count=0;
-	int index=1;
-	x=getlength(ch,ptr,count);
-	y=finddecimal(ch,ptr,index);
-	cout <<"length is :"<<x<<endl;
-	cout<<"decimal is at position :"<<y<<endl;
-	return 0;
+    char *ptr = NULL;
+    ptr = ch;
+    int x, y; // Variables to store the length and decimal position
+    int count = 0; // Counter for length calculation
+    int index = 1; // Index for decimal position calculation
+
+    // Call the functions to get the length and decimal position
+    x = getlength(ch, ptr, count);
+    y = finddecimal(ch, ptr, index);
+
+    // Print the results
+    cout << "Length is: " << x << endl;
+    cout << "Decimal is at position: " << y << endl;
+
+    return 0;
 }
